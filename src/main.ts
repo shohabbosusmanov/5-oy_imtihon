ee085cce97f65adbbb042d6ff3023b3efb5c08d7
Shohabbos Usmanov <shohabbosusmanov@gmail.com>
2025-06-20 16:59:46 +0500

    s

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,5 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ResponseInterceptor(reflector));
   await app.listen(process.env.PORT ?? 3000);
-  console.log(1);
 }
 bootstrap();
